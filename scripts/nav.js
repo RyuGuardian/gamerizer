ba516fbea61ab78dc4efaa68e930c290e0938f34
Austin King <aust.a.king@gmail.com>
2016-04-06 21:00:39 -0700

    Got rid of 'generic' labels and replaced them with 'gamerizer', since front page will have general randomizing functions.

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,5 +1,9 @@
 //Click on icon to slide in modal. Click on option in modal to change view.
 
+function namify(string) {
+  return string.split('-').map(function(val) {return val[0].toUpperCase() + val.slice(1)}).join(' ');
+}
+
 $('#nav-icon').on('click', function() {
   console.log('nav click');
   $navMenu = $('nav');
@@ -8,15 +12,16 @@ $('#nav-icon').on('click', function() {
 });
 
 $('nav').on('click', 'li', function() {
-  var $navSelection = $(this).data('selection');
+  var navSelection = $(this).data('selection');
 
   $('nav').hide().css('right', '-50%')
-  $('header').removeClass().addClass($navSelection + '-header-color');
+  $('header').removeClass().addClass(navSelection + '-header-color');
+  $('header > h1').text(namify(navSelection));
   $('main').empty();
 
-  if($navSelection !== 'generic') {
-    $('main').load('html/' + $navSelection + '.html', function() {
-      $.getScript('scripts/' + $navSelection + '.js');
+  if(navSelection !== 'gamerizer') {
+    $('main').load('html/' + navSelection + '.html', function() {
+      $.getScript('scripts/' + navSelection + '.js');
     });
   }
 });
