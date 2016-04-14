939646a0e67f1c11135dfc9d09ebee3f21444e1f
Austin King <aust.a.king@gmail.com>
2016-04-13 20:54:50 -0700

    Implemented random rule functionality, including a small style for spacing. Started on spinner functionality, in that an element is inserted where it will be when the option is selected.

--- scripts/gamerizer.js
+++ scripts/gamerizer.js
@@ -1,14 +1,40 @@
 $('.game-turn-type').on('click', 'button', function() {
   $(this).parent().siblings().children().removeClass('selected');
-  $('.decide-first button').removeClass('selected');
   $(this).addClass('selected');
+  $('.decide-first button').removeClass('selected');
+  $('.decide-first ul').next().remove();
 
   $('.gamerizer > section').hide();
   $('.' + $(this).data('turns')).fadeIn();
 });
 
+var rules = [
+  "is the oldest",
+  "is the youngest",
+  "is tallest",
+  "is shortest",
+  "has the longest hair",
+  "has the shortest hair",
+  "has been sick most recently",
+  "had a birthday most recently",
+  "has their birthday next",
+  "left the country most recently",
+  "went on vacation most recently"
+  ];
+
 $('.decide-first').on('click', 'button', function() {
   $(this).parent().siblings().children().removeClass('selected');
   $(this).addClass('selected');
+
+  var listElement = $('.decide-first ul');
+
+  listElement.next().remove();
+
+  if($(this).data('first') === 'rule') {
+    listElement.after("<p>The person who " + rules[Math.floor(Math.random() * rules.length)] + " is first.</p>");
+  }
+  else if($(this).data('first') === 'spinner') {
+    listElement.after('<div class="spinner"></div>')
+  }
 
 });
