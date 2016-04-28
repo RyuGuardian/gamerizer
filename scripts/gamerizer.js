d86d9564bc63975e52fe979ea3668d8573d539f0
Austin King <aust.a.king@gmail.com>
2016-04-28 16:16:15 -0700

    Added randomized color list and styled it.

--- scripts/gamerizer.js
+++ scripts/gamerizer.js
@@ -6,6 +6,38 @@ $('.game-turn-type').on('click', 'button', function() {
 
   $('.gamerizer > section').hide();
   $('.' + $(this).data('turns')).fadeIn();
+
+  if($(this).data('turns') === 'random-order') {
+    var colors = [
+      { red: { background: '#ff0000', text: '#800000'} },
+      { green: { background: '#228b22', text: '#004000'} },
+      { blue: { background: '#1e90ff', text: '#0000aa'} },
+      { yellow: { background: '#FFFF2A', text: '#A88E00'} },
+      { orange: { background: '#ffa500', text: '#ff4500'} },
+      { purple: { background: '#8a2be2', text: '#4b0082' }},
+      { black: { background: '#000000', text: '#101050'} },
+      { white: { background: '#ffffff', text: '#deb080'} },
+      { grey: { background: '#c0c0c0', text: '#404040'} }
+    ];
+
+    $('.random-order > ol > li').remove();
+
+    while(colors.length) {
+      var randomIndex = Math.floor(Math.random() * colors.length);
+      var selectedColor = Object.keys(colors[randomIndex])[0];
+
+      $('.random-order > ol').append('<li><p>' + selectedColor.toUpperCase() + '</p></li>');
+      $('.random-order li:last-of-type > p')
+        .css('background-color', colors[randomIndex][selectedColor].background);
+      $('.random-order li:last-of-type > p').css('color', colors[randomIndex][selectedColor].text);
+
+      for(var i = randomIndex; i < colors.length; i++) {
+        colors[i] = colors[i + 1];
+      }
+
+      colors.length--;
+    }
+  }
 });
 
 (function(window) {
@@ -31,7 +63,6 @@ $('.decide-first').on('click', 'button', function() {
   var listElement = $('.decide-first ul');
   listElement.next().remove();
 
-
   if($(this).data('first') === 'rule') {
     $('.spinner').hide();
     listElement.after("<p>The person who "
@@ -42,5 +73,4 @@ $('.decide-first').on('click', 'button', function() {
     $('.spinner').show();
     $.getScript('scripts/spinner.js');
   }
-
 });
